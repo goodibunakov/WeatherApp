53b7aa07fa81f63a869d31af0fa8a092493fcb36
Evgeny GooDi Bunakov <goodibunakov@gmail.com>
2017-02-18 18:35:11 +0700

    закончил верстку UI

--- app/src/main/java/ru/goodibunakov/weatherapp/MainActivity.java
+++ app/src/main/java/ru/goodibunakov/weatherapp/MainActivity.java
@@ -1,7 +1,11 @@
 package ru.goodibunakov.weatherapp;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        TextView temp = (TextView) findViewById(R.id.temperature);
+        temp.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/HelveticaNeueCyr-UltraLight.otf"));
+        TextView sky = (TextView) findViewById(R.id.sky);
+        sky.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/weather.ttf"));
+        TextView gradus = (TextView) findViewById(R.id.gradus);
+        gradus.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/HelveticaNeueCyr-UltraLight.otf"));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
