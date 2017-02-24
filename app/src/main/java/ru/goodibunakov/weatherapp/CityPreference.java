02580add19b3ca6a993f5be477daaf36aa0c03e8
Evgeny GooDi Bunakov <goodibunakov@gmail.com>
2017-02-24 22:30:18 +0700

    всё готово. всё работает ;)

---
+++ app/src/main/java/ru/goodibunakov/weatherapp/CityPreference.java
@@ -0,0 +1,25 @@
+package ru.goodibunakov.weatherapp;
+
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+
+class CityPreference {
+
+    //Вспомогательный класс для хранения выбранного города
+    SharedPreferences prefs;
+
+    CityPreference(Activity activity) {
+        prefs = activity.getPreferences(Activity.MODE_PRIVATE);
+    }
+
+    // Возвращаем город по умолчанию, если SharedPreferences пустые
+    String getCity() {
+        return prefs.getString("city", "Tomsk");
+    }
+
+    void setCity(String city) {
+        prefs.edit().putString("city", city).apply();
+    }
+}
+
